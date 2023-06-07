a7ec3789b440e1b01cedf844ceae4752fdf2e959
Prince <90514663+prince5032@users.noreply.github.com>
2023-06-07 19:53:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express  = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const colors = require('colors');
-const connectDB = require('./config/connextDB');
+const express  = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
 const path = require("path");
+const connectDB = require("./config/connectDB");
+
 
 
 dotenv.config();
@@ -15,15 +16,15 @@ connectDB();
 const app = express();
 
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
 // app.get('/', (req,res)=>{
 //     res.send("<h1>hello from server</h1>")
 // });
-app.use('/api/v1/users', require("./routes/userRoute"));
-app.use('/api/v1/transections', require("./routes/transectionRoute"));
+app.use("/api/v1/users", require("./routes/userRoute"));
+app.use("/api/v1/transections", require("./routes/transectionRoute"));
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
