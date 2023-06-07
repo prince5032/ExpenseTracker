a9ce6ac1ed1264d070ff09a08011e793292edbe6
Prince <90514663+prince5032@users.noreply.github.com>
2023-06-07 19:49:40 +0530

    Update connectDB.js

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,5 +1,5 @@
-const mongoose  = require('mongoose');
-const colors  = require('colors');
+const mongoose  = require("mongoose");
+const colors  = require("colors");
 const connectDB = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URL);
@@ -7,6 +7,6 @@ const connectDB = async ()=>{
     } catch (error){
         console.log(`${error}`.bgRed)
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
